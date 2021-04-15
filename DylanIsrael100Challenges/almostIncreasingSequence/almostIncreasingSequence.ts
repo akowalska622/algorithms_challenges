d52cbd73a2eb2cd3073f2e3fc8f3d4baebe89f3a
akowalska622 <63072419+akowalska622@users.noreply.github.com>
2021-04-15 10:28:45 +0200

    Dylan challenge

--- DylanIsrael100Challenges/almostIncreasingSequence/almostIncreasingSequence.ts
+++ DylanIsrael100Challenges/almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,5 +1,14 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
-
+	let count = 0 //how many times the sequence is out of sync
+  
+  for (let i = 0; i < sequence.length; i++) {
+  	if (sequence[i] <= sequence[i-1]) count ++
+    if (sequence[i] <= sequence[i-1] && sequence[i+1] <= sequence[i-1]) {
+    	return false
+    }
+  }
+  
+  return count <= 1
 }
 
 console.log(almostIncreasingSequence([1, 3, 2, 1])) 
