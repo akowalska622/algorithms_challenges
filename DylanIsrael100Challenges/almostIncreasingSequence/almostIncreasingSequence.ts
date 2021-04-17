47ed1d7e0ac40e3d0026714a20df3c545dabfa83
Anna Kowalska <a.kowalska622@gmail.com>
2021-04-17 14:28:49 +0200

    7 kyu

--- DylanIsrael100Challenges/almostIncreasingSequence/almostIncreasingSequence.ts
+++ DylanIsrael100Challenges/almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,15 +1,15 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
 	let count = 0 //how many times the sequence is out of sync
-  
+
   for (let i = 0; i < sequence.length; i++) {
   	if (sequence[i] <= sequence[i-1]) count ++
     if (sequence[i] <= sequence[i-1] && sequence[i+1] <= sequence[i-1]) {
     	return false
     }
   }
-  
+
   return count <= 1
 }
 
-console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1]))
+console.log(almostIncreasingSequence([1, 3, 2]))
