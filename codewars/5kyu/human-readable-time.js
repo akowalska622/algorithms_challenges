c2d789ff232f81ff5439f4f0be36b598d9f27bf7
Anna Kowalska <a.kowalska622@gmail.com>
2021-04-26 18:04:40 +0200

    human readable time improvements

--- codewars/5kyu/human-readable-time.js
+++ codewars/5kyu/human-readable-time.js
@@ -8,31 +8,45 @@ The maximum time never exceeds 359999 (99:59:59)
 
 You can find some examples in the test fixtures.
 */
-function humanReadable(seconds) {
-  let hours = 0;
-  let minutes = 0;
-  let s = 0;
 
-  if (seconds <= 60) s = seconds;
+// FIRST ATTEMPT
+// function humanReadable(seconds) {
+//   let hours = 0;
+//   let minutes = 0;
+//   let s = 0;
 
-  if (seconds <= 3600) {
-    minutes = Math.floor(seconds / 60);
-    s = seconds - minutes * 60;
-  }
+//   if (seconds < 60) s = seconds;
 
-  if (seconds > 3600) {
-    hours = Math.floor(seconds / 3600);
-    let secondsLeft = seconds - hours * 3600;
+//   if (seconds < 3600) {
+//     minutes = Math.floor(seconds / 60);
+//     s = seconds - minutes * 60;
+//   }
 
-    minutes = Math.floor(secondsLeft / 60);
+//   if (seconds >= 3600) {
+//     hours = Math.floor(seconds / 3600);
+//     let secondsLeft = seconds - hours * 3600;
 
-    s = secondsLeft - minutes * 60;
-  }
+//     minutes = Math.floor(secondsLeft / 60);
+
+//     s = secondsLeft - minutes * 60;
+//   }
+
+//   return `${hours < 10 ? '0' + hours : hours}:${
+//     minutes < 10 ? '0' + minutes : minutes
+//   }:${s < 10 ? '0' + s : s}`;
+// }
+
+//SECOND ATTEMPT
+
+const humanReadable = seconds => {
+  let hours = Math.floor(seconds / 3600);
+  let minutes = Math.floor((seconds / 60) % 60);
+  let s = seconds % 60;
 
   return `${hours < 10 ? '0' + hours : hours}:${
     minutes < 10 ? '0' + minutes : minutes
   }:${s < 10 ? '0' + s : s}`;
-}
+};
 
 console.log(humanReadable(0)); //'00:00:00',
 console.log(humanReadable(5)); //'00:00:05',
