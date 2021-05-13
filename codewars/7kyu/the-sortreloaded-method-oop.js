fbb8f6bed74ce8f96c2ae39b92bb8a20b2749d42
akowalska622 <63072419+akowalska622@users.noreply.github.com>
2021-05-13 10:56:33 +0200

    Update the-sortreloaded-method-oop.js

--- codewars/7kyu/the-sortreloaded-method-oop.js
+++ codewars/7kyu/the-sortreloaded-method-oop.js
@@ -6,10 +6,12 @@ In this Kata you have to extend the Array object and add the sortReloaded(dir) m
 It should receive a dir parameter which has with two possible values 'asc' or 'desc', and return a new array ordered ascendingly or descendingly respectively. If no dir parameter is set it should assume 'asc' by default. If it has an invalid value, return false.
 
 For the effects of this Kata, all the arrays will contain only Integer numbers so you don't have to care about validating them.
+Should not modify the original array.
+https://www.codewars.com/kata/5610a8eeb9a84d624b000005/train/javascript
 */
 
 Array.prototype.sortReloaded = function(dir = "asc") {
-  if (dir === "asc") return this.sort((a, b) => a-b)
-  if (dir === "desc") return this.sort((a, b) => b-a)
+  if (dir === "asc") return [...this].sort((a, b) => a-b)
+  if (dir === "desc") return [...this].sort((a, b) => b-a)
   else return false
 }
