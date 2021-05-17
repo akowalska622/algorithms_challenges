6d53f34ad712754adf9a4af052555ee3bc456d2f
akowalska622 <63072419+akowalska622@users.noreply.github.com>
2021-05-17 10:52:06 +0200

    Create numbers-to-letters.js

---
+++ codewars/7kyu/numbers-to-letters.js
@@ -0,0 +1,12 @@
+/*
+Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+
+All inputs will be valid.
+
+https://www.codewars.com/kata/57ebaa8f7b45ef590c00000c/train/javascript
+*/
+
+function switcher(x){
+  let alphabet = ['z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a', '!', '?', ' ']
+  return x.map(letter => alphabet[+letter-1]).join("")
+}
