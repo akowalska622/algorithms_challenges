1543eb9930ae5e6f5019f872eb9cd9ac6a3c9c5e
akowalska622 <63072419+akowalska622@users.noreply.github.com>
2021-05-20 13:01:52 +0200

    Create podcast-speed-listening-calculator.js

---
+++ codewars/7kyu/podcast-speed-listening-calculator.js
@@ -0,0 +1,24 @@
+/*
+If you have ever listened to an audiobook or podcast, you may have had the option to change the play speed of the audio. This allows you to increase or decrease the speed that you listen to your content.
+
+I want you to calculate the time it takes to listen to the audio when the play speed is varied. The time should be rounded down to whole seconds.
+
+The inputs will be:
+
+audioLength: a string of the total time of the audio, "hh:mm:ss" 
+
+playSpeed: a float between 0.5 and 3.0, only to 1 decimal point 
+
+Examples:
+The results of calculations on the time units are expected to be truncated/floored
+
+speedListen("00:00:55", 2) => "00:00:27" // NOT "00:00:28"
+
+speedListen("01:20:00", 1.5) => "00:53:20"
+https://www.codewars.com/kata/5a08bb3c80171f4b040000fc/javascript
+*/
+
+function speedListen(t,s) {
+    t = t.split(':').reduce((t,v) => +v+t*60,0)/s|0;
+    return [t/3600|0,(t/60|0)%60,t%60].map(v => (''+v).padStart(2,'0')).join(':');
+}
