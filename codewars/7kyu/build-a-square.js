eaab82768d46f91bdfc899de84f18d6859d31443
Anna Kowalska <a.kowalska622@gmail.com>
2021-05-22 20:43:10 +0200

    7kyu

---
+++ codewars/7kyu/build-a-square.js
@@ -0,0 +1,21 @@
+/*
+I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+
+Example
+n = 3, so I expect a 3x3 square back just like below as a string:
+
++++
++++
++++
+
+https://www.codewars.com/kata/59a96d71dbe3b06c0200009c/train/javascript
+*/
+
+function generateShape(integer) {
+  let str = '';
+  for (let i = 0; i < integer; i++) {
+    str += '+'.repeat(integer) + '\n';
+  }
+
+  return str.slice(0, -1);
+}
