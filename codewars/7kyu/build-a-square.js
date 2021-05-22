489442f2f4959e65c5b20a7dbce97075fee11f76
Anna Kowalska <a.kowalska622@gmail.com>
2021-05-22 20:46:09 +0200

    7kyu

--- codewars/7kyu/build-a-square.js
+++ codewars/7kyu/build-a-square.js
@@ -11,11 +11,15 @@ n = 3, so I expect a 3x3 square back just like below as a string:
 https://www.codewars.com/kata/59a96d71dbe3b06c0200009c/train/javascript
 */
 
-function generateShape(integer) {
-  let str = '';
-  for (let i = 0; i < integer; i++) {
-    str += '+'.repeat(integer) + '\n';
-  }
+// function generateShape(integer) {
+//   let str = '';
+//   for (let i = 0; i < integer; i++) {
+//     str += '+'.repeat(integer) + '\n';
+//   }
 
-  return str.slice(0, -1);
+//   return str.slice(0, -1);
+// }
+
+function generateShape(integer) {
+  return `${'+'.repeat(integer)}\n`.repeat(integer).slice(0, -1);
 }
